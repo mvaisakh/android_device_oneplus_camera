4b668d322b414ffed38562970c60d9ef56c59a55
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-25 12:21:16 +0000

    op-camera: Add minimal OplusBuild class
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ oplus-fwk/src/com/oplus/os/OplusBuild.java
@@ -0,0 +1,11 @@
+package com.oplus.os;
+
+public class OplusBuild {
+    public static class VERSION {
+        public static final String RELEASE = "V12.1";
+    }
+
+    public static int getOplusOSVERSION() {
+        return 27;
+    }
+}
