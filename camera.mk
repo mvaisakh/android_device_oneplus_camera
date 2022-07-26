7163c58dc6164f5cf4403dc364635ddc78dd17f8
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-26 12:42:24 +0000

    op-camera: Add more default grant perms
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- camera.mk
+++ camera.mk
@@ -9,6 +9,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += oplus-fwk
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml
+    $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml \
+    $(LOCAL_PATH)/permissions-oplus-camera-extra.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions-oplus-camera-extra.xml
 
 $(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
