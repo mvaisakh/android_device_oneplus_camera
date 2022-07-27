a0c028debb6427566f5a3d55f0bcc57e07b59f9a
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 06:50:03 +0000

    op-camera: Fixup xml formatting
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- extract-utils.sh
+++ extract-utils.sh
@@ -55,18 +55,8 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib/libsample1.so)
-            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
-            ;;
-        vendor/lib64/libsample2.so)
-            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
-            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
-            ;;
-        vendor/lib/libsample5.so)
-            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
-            ;;
-        vendor/lib/libsample7.so)
-            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
+        product/etc/permissions/com.oplus.camera.unit.sdk_product.xml)
+            sed -i 's|?\ xml|?xml|g' "${2}"
             ;;
     esac
 }
