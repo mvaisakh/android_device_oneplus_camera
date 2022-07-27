1a0f39854f3f0658ef6a275c14bff7bb3a01703e
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 11:07:48 +0000

    opluscameraframework: Implement static getInstance() for OPlusAccessControlManager
    Referred by the unit sdk
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- opluscameraframework/src/com/oplus/app/OPlusAccessControlManager.java
+++ opluscameraframework/src/com/oplus/app/OPlusAccessControlManager.java
@@ -4,6 +4,21 @@ import android.os.UserHandle;
 
 public class OPlusAccessControlManager {
     public static final int USER_CURRENT = UserHandle.myUserId();
+    private static volatile OPlusAccessControlManager sInstance = null;
+
+    private OPlusAccessControlManager() {
+    }
+
+    public static OPlusAccessControlManager getInstance() {
+        if (sInstance == null) {
+            synchronized (OPlusAccessControlManager.class) {
+                if (sInstance == null) {
+                    sInstance = new OPlusAccessControlManager();
+                }
+            }
+        }
+        return sInstance;
+    }
 
     public boolean isEncryptPass(String packageName, int userId) {
         return true;
