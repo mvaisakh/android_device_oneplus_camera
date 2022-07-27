ef44b3a875501d070efe7596da795cbd1fa2fa18
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 06:33:33 +0000

    op-camera: Add aux packagelist props
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- camera.mk
+++ camera.mk
@@ -12,4 +12,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml \
     $(LOCAL_PATH)/permissions-oplus-camera-extra.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/default-permissions/permissions-oplus-camera-extra.xml
 
+OPLUS_CAMERA_PROPERTIES := \
+    vendor.camera.aux.packagelist=android,com.oplus.camera \
+    persist.vendor.camera.privapp.list=com.oplus.camera \
+    persist.camera.privapp.list=com.oplus.camera
+
+PRODUCT_VENDOR_PROPERTIES += $(OPLUS_CAMERA_PROPERTIES)
+PRODUCT_PRODUCT_PROPERTIES += $(OPLUS_CAMERA_PROPERTIES)
+
 $(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
