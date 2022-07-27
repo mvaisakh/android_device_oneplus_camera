ee7d52fb4822c4539a7ec5f85579f1316174a9d1
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 06:34:12 +0000

    opluscameraframework: Implement minimal OplusBaseConfiguration
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ opluscameraframework/src/android/content/res/OplusBaseConfiguration.java
@@ -0,0 +1,11 @@
+package android.content.res;
+
+import oplus.content.res.OplusExtraConfiguration;
+
+public abstract class OplusBaseConfiguration {
+    public OplusExtraConfiguration mOplusExtraConfiguration = null;
+
+    public OplusExtraConfiguration getOplusExtraConfiguration() {
+        return this.mOplusExtraConfiguration;
+    }
+}
