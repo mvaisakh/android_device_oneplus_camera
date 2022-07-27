4e3c4d0a30699035fd2948ac029cedde6d032e83
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 06:36:32 +0000

    opluscameraframework: Implement minimal OplusTypeCastingHelper
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ opluscameraframework/src/com/oplus/util/OplusTypeCastingHelper.java
@@ -0,0 +1,9 @@
+package com.oplus.util;
+
+import android.content.Context;
+
+public final class OplusTypeCastingHelper {
+    public static <T> T typeCasting(Class<T> type, Context context) {
+        return null;
+    }
+}
