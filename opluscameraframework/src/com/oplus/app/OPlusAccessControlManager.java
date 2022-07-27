7db9181b0fae30bdc006e8146cbd36eb5f2d0fe1
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-27 07:33:10 +0000

    opluscameraframework: Implement minimal OplusAccessControlManager
    Camera only calls for isEncryptedPackage() and isEncryptedPass()
    Return true by default for both
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ opluscameraframework/src/com/oplus/app/OPlusAccessControlManager.java
@@ -0,0 +1,12 @@
+package com.oplus.app;
+
+public class OPlusAccessControlManager {
+
+    public boolean isEncryptPass(String packageName, int userId) {
+        return true;
+    }
+
+    public boolean isEncryptedPackage(String packageName, int userId) {
+        return true;
+    }
+}
