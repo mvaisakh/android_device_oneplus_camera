28d6cd70a3271062999e0155d7b8e7183d9de43f
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-15 08:04:53 +0000

    opluscameraframework: Add USER_CURRENT field
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- opluscameraframework/src/com/oplus/app/OPlusAccessControlManager.java
+++ opluscameraframework/src/com/oplus/app/OPlusAccessControlManager.java
@@ -1,6 +1,9 @@
 package com.oplus.app;
 
+import android.os.UserHandle;
+
 public class OPlusAccessControlManager {
+    public static final int USER_CURRENT = UserHandle.myUserId();
 
     public boolean isEncryptPass(String packageName, int userId) {
         return true;
