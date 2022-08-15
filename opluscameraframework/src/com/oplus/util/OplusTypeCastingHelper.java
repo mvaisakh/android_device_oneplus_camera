e997e74e315c48098fd98ccb5b36b046b3bbd869
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-15 08:05:55 +0000

    opluscameraframework: Drop typecasting method
    The exception for this is caught in the app
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- opluscameraframework/src/com/oplus/util/OplusTypeCastingHelper.java
+++ opluscameraframework/src/com/oplus/util/OplusTypeCastingHelper.java
@@ -3,7 +3,4 @@ package com.oplus.util;
 import android.content.Context;
 
 public final class OplusTypeCastingHelper {
-    public static <T> T typeCasting(Class<T> type, Context context) {
-        return null;
-    }
 }
