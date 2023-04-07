88336826ba91d87c54758c046b73f422c46f265a
Alexander Koskovich <akoskovich@pm.me>
2023-04-08 01:56:23 +0600

    oplus-camera: initial stub OplusFeatureConfigManager class
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- camera_product.mk
+++ camera_product.mk
@@ -5,6 +5,12 @@
 # OPlus Camera product specifications
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/priv-app-permissions-oplus-camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-oplus-camera.xml
+    $(LOCAL_PATH)/priv-app-permissions-oplus-camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-oplus-camera.xml \
+    $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml
+
+PRODUCT_PACKAGES += \
+    opluscameraframework
+
+PRODUCT_BOOT_JARS += opluscameraframework
 
 $(call inherit-product, vendor/oplus/camera/camera-vendor.mk)
---
+++ opluscameraframework/src/com/oplus/content/OplusFeatureConfigManager.java
@@ -0,0 +1,23 @@
+//
+// Copyright (C) 2023 StatiXOS
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package com.oplus.content;
+
+import android.os.RemoteException;
+
+public class OplusFeatureConfigManager {
+    private static OplusFeatureConfigManager sInstance = null;
+
+    public static OplusFeatureConfigManager getInstance() {
+        if (sInstance == null) {
+            sInstance = new OplusFeatureConfigManager();
+        }
+        return sInstance;
+    }
+
+    boolean hasFeature() throws RemoteException {
+        return false;
+    }
+}
