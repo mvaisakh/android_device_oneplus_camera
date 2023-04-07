17c153a379575c62dcfeb4762dd140a5087948a9
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-07 23:16:15 +0600

    oplus-camera: add a stripped down priv-app permissions
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- camera_product.mk
+++ camera_product.mk
@@ -4,4 +4,7 @@
 
 # OPlus Camera product specifications
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/priv-app-permissions-oplus-camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-oplus-camera.xml
+
 $(call inherit-product, vendor/oplus/camera/camera-vendor.mk)
