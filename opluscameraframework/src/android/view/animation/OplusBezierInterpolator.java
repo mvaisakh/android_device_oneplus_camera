24b9194b5c77640f1fc7b3a7f54f47e67ef533b6
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-08 11:53:55 +0600

    opluscameraframework: add a stub OplusBezierInterpolator
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ opluscameraframework/src/android/view/animation/OplusBezierInterpolator.java
@@ -0,0 +1,6 @@
+package android.view.animation;
+
+public class OplusBezierInterpolator {
+    
+    public OplusBezierInterpolator(double p1x, double p1y, double p2x, double p2y, boolean limit) {}
+}
