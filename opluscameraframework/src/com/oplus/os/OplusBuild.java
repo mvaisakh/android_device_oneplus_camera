692f18f62f2773916e206abce257b662f0553dd3
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-08 12:28:03 +0600

    opluscameraframework: uprev build release to 13.0
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- opluscameraframework/src/com/oplus/os/OplusBuild.java
+++ opluscameraframework/src/com/oplus/os/OplusBuild.java
@@ -2,7 +2,7 @@ package com.oplus.os;
 
 public class OplusBuild {
     public static class VERSION {
-        public static final String RELEASE = "V12.1";
+        public static final String RELEASE = "V13.0";
     }
 
     public static int getOplusOSVERSION() {
