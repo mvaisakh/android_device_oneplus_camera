0f3be5fcc51463cbc214816e13118778d4c2bea0
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-09 12:14:17 +0600

    oplus-camera: move everything back to product
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- camera_product.mk
+++ camera_product.mk
@@ -5,9 +5,9 @@
 # OPlus Camera product specifications
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/priv-app-permissions-oplus-camera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus-camera.xml \
-    $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml \
-    $(LOCAL_PATH)/default_grant_permissions_list_oplus_camera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/default_grant_permissions_list_oplus_camera.xml
+    $(LOCAL_PATH)/priv-app-permissions-oplus-camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-oplus-camera.xml \
+    $(LOCAL_PATH)/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml \
+    $(LOCAL_PATH)/default_grant_permissions_list_oplus_camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/default_grant_permissions_list_oplus_camera.xml
 
 PRODUCT_PACKAGES += \
     opluscameraframework
