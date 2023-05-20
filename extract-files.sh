63557e2e5dd1a86d0c877533b00df77769b5f9e6
Vaisakh Murali <mvaisakh@statixos.com>
2023-05-20 10:37:34 +0600

    oplus-camera: ship oplus camera unit SDK
    Change-Id: I96ed0591bcb752aa1bf39ce84812ad1cca2e2d56
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        product/etc/permissions/com.oplus.camera.unit.sdk_product.xml)
+            sed -i 's|?\ xml|?xml|g' "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
